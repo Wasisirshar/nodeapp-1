0aebf26c28e3b2114ad83e80b003652ed6d83da6
Wasisirshar <sirsharyarbaig@gmail.com>
2022-09-01 19:32:03 +0500

    added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log(`hello world`);
 
 console.log(123)
+
+//git status  hamay status batata hai k hamny kab commit krna hai matlab ager koui file ma chngeing aye hay tou haay git ko commit krne ko bolyga
+
+// //pheleiu dafa lana ho ager file to ham git clone and liunk dekey kam kar krty 
+
+// cat cmd files ko read krney k lia hikta hikta
+
+// git branch say ham kis brnach pai hai hamay malom hota hai
+
+// git checkout -b andName deney say hamara new branch local machine paiu bnn jtra hai
+
